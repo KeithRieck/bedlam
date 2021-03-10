cbe83e40ec74c303aad13062af170a190aa8262a
Keith Rieck <KeithRieck@gmail.com>
2021-03-09 20:42:12 -0600

    Added Animations and Spritesheets

--- build/demo.js
+++ build/demo.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-12-13 18:02:26
+// Transcrypt'ed from Python, 2021-03-09 20:41:03
 import {AssertionError, AttributeError, BaseException, DeprecationWarning, Exception, IndexError, IterableError, KeyError, NotImplementedError, RuntimeWarning, StopIteration, UserWarning, ValueError, Warning, __JsIterator__, __PyIterator__, __Terminal__, __add__, __and__, __call__, __class__, __envir__, __eq__, __floordiv__, __ge__, __get__, __getcm__, __getitem__, __getslice__, __getsm__, __gt__, __i__, __iadd__, __iand__, __idiv__, __ijsmod__, __ilshift__, __imatmul__, __imod__, __imul__, __in__, __init__, __ior__, __ipow__, __irshift__, __isub__, __ixor__, __jsUsePyNext__, __jsmod__, __k__, __kwargtrans__, __le__, __lshift__, __lt__, __matmul__, __mergefields__, __mergekwargtrans__, __mod__, __mul__, __ne__, __neg__, __nest__, __or__, __pow__, __pragma__, __proxy__, __pyUseJsNext__, __rshift__, __setitem__, __setproperty__, __setslice__, __sort__, __specialattrib__, __sub__, __super__, __t__, __terminal__, __truediv__, __withblock__, __xor__, abs, all, any, assert, bool, bytearray, bytes, callable, chr, copy, deepcopy, delattr, dict, dir, divmod, enumerate, filter, float, getattr, hasattr, input, int, isinstance, issubclass, len, list, map, max, min, object, ord, pow, print, property, py_TypeError, py_iter, py_metatype, py_next, py_reversed, py_typeof, range, repr, round, set, setattr, sorted, str, sum, tuple, zip} from './org.transcrypt.__runtime__.js';
 import {Sprite} from './bedlam.js';
 import {Scene} from './bedlam.js';
@@ -6,8 +6,9 @@ import {ImageSprite} from './bedlam.js';
 import {GameTask} from './bedlam.js';
 import {Game} from './bedlam.js';
 import {Button} from './bedlam.js';
+import {Animation} from './bedlam.js';
 var __name__ = '__main__';
-export var Ball =  __class__ ('Ball', [Sprite], {
+export var BallSprite =  __class__ ('BallSprite', [Sprite], {
 	__module__: __name__,
 	get __init__ () {return __get__ (this, function (self, game, speed, radius) {
 		if (typeof speed == 'undefined' || (speed != null && speed.hasOwnProperty ("__kwargtrans__"))) {;
@@ -54,25 +55,28 @@ export var Ball =  __class__ ('Ball', [Sprite], {
 		ctx.restore ();
 	});}
 });
-export var Duke =  __class__ ('Duke', [ImageSprite], {
+export var DukeSprite =  __class__ ('DukeSprite', [ImageSprite], {
 	__module__: __name__,
 	get __init__ () {return __get__ (this, function (self, game, speed) {
 		if (typeof speed == 'undefined' || (speed != null && speed.hasOwnProperty ("__kwargtrans__"))) {;
 			var speed = 20;
 		};
 		var duke_image = game.load_image ('dukeImage');
-		ImageSprite.__init__ (self, game, 50, 50, duke_image);
+		ImageSprite.__init__ (self, game, duke_image, 50, 50);
 		self.x = 100;
 		self.y = 100;
-		var angle = (2 * Math.PI) * Math.random ();
-		self.dx = speed * Math.cos (angle);
-		self.dy = speed * Math.sin (angle);
+		var d_angle = (2 * Math.PI) * Math.random ();
+		self.dx = speed * Math.cos (d_angle);
+		self.dy = speed * Math.sin (d_angle);
+		self.angle = 0;
 		self.radius = self.width / 2;
 		self.originX = self.radius;
 		self.originY = self.radius;
+		self.game_image.originX = self.originX;
+		self.game_image.originY = self.originY;
+		self.game_image.angle = self.angle;
 	});},
-	get py_update () {return __get__ (this, function (self, delta_time) {
-		self.angle = self.angle + 0.4;
+	get _move () {return __get__ (this, function (self, delta_time) {
 		self.x = self.x + (self.dx * delta_time) / 1000.0;
 		if (self.x < self.radius) {
 			self.x = self.radius;
@@ -91,6 +95,27 @@ export var Duke =  __class__ ('Duke', [ImageSprite], {
 			self.y = self.game.canvas.height - self.radius;
 			self.dy = self.dy * -(1.0);
 		}
+	});},
+	get py_update () {return __get__ (this, function (self, delta_time) {
+		ImageSprite.py_update (self, delta_time);
+		self.angle = self.angle + 0.4;
+		self._move (delta_time);
+	});}
+});
+export var ScrollerSprite =  __class__ ('ScrollerSprite', [DukeSprite], {
+	__module__: __name__,
+	get __init__ () {return __get__ (this, function (self, game, animation, speed) {
+		if (typeof speed == 'undefined' || (speed != null && speed.hasOwnProperty ("__kwargtrans__"))) {;
+			var speed = 20;
+		};
+		DukeSprite.__init__ (self, game, speed);
+		self.width = 60;
+		self.height = 60;
+		self.radius = self.width / 2;
+		animation.originX = self.radius;
+		animation.originY = self.radius;
+		self.animation = animation;
+		self.animation.start ();
 	});}
 });
 export var DemoScene =  __class__ ('DemoScene', [Scene], {
@@ -102,7 +127,7 @@ export var DemoScene =  __class__ ('DemoScene', [Scene], {
 		Scene.__init__ (self, game, py_name);
 		self.my_sound = game.load_audio ('mySound');
 		for (var n = 0; n < num_balls; n++) {
-			self.append (Ball (self.game, 100, 10));
+			self.append (BallSprite (self.game, 100, 10));
 		}
 	});},
 	get handle_mousedown () {return __get__ (this, function (self, event) {
@@ -213,14 +238,17 @@ export var DemoScene3 =  __class__ ('DemoScene3', [DemoScene], {
 			var num_balls = 8;
 		};
 		DemoScene.__init__ (self, game, py_name, num_balls);
-		self.append (Duke (self.game, 50));
+		var scroller_spritesheet = game.load_spritesheet ('assets/scroller.json', 'scrollerImage');
+		var animation = Animation (self.game, scroller_spritesheet.frames ());
+		animation.py_name = 'scroller';
+		self.append (DukeSprite (self.game, 50));
+		self.append (ScrollerSprite (self.game, animation, 50));
 		self.background_image = self.game.load_image ('backgroundImage');
+		self.background_image.global_composition_operation = 'copy';
 	});},
 	get _clear_screen () {return __get__ (this, function (self, ctx) {
-		ctx.save ();
-		ctx.globalCompositeOperation = 'copy';
-		ctx.drawImage (self.background_image, 0, 0);
-		ctx.restore ();
+		DemoScene._clear_screen (self, ctx);
+		self.background_image.draw (ctx);
 	});}
 });
 export var BallsGame =  __class__ ('BallsGame', [Game], {
--- build/org.transcrypt.__runtime__.js
+++ build/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-12-13 18:02:26
+// Transcrypt'ed from Python, 2021-03-09 20:41:03
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
