4ad92071040f77d20ff8eb9e485d427d167c5436
Keith Rieck <KeithRieck@gmail.com>
2021-06-28 22:36:30 -0500

    Add scene to demo for mouse events

--- demo.py
+++ demo.py
@@ -120,8 +120,10 @@ class DemoScene(Scene):
         self.button2.enabled = not self.button2.enabled
         console.log("TEST: button function :")
         t = 4
+
         def ouch_closure():
-            console.log("TEST: closure: " + t + " seconds later ")
+            console.log("TEST: closure: {} seconds later ".format(t))
+
         lambda_task_1 = lambda: console.log("TEST: lambda, two seconds in the future: ")
         self.schedule(lambda_task_1, 2000)
         self.schedule(self.ouch_func, 3000)
@@ -149,11 +151,13 @@ class OuchTask2(GameTask):
     def __init__(self, game, gameobject, time_delay=0, repeat_count=0, msg="HELLO"):
         GameTask.__init__(self, game, gameobject, None, time_delay, repeat_count)
         self.count = 0
+
         def run_func():
             self.count = self.count + 1
             new_fill_color = 'red' if self.count % 2 == 1 else 'white'
             self.gameobject.button2.fillStyle = new_fill_color
-            console.log("TEST: custom GameTask with a closure inside, : " + self.count + " : " + msg)
+            console.log("TEST: custom GameTask with a closure inside, : {}} : {}".format(self.count, msg))
+
         self.func = run_func
 
 
@@ -196,34 +200,182 @@ class DemoScene3(DemoScene):
         self.background_image.draw(ctx)
 
 
-class BallsGame(Game):
+class DemoScene4(DemoScene):
+    def __init__(self, game, name, num_balls=4):
+        DemoScene.__init__(self, game, name, num_balls)
+        self.font = '12pt sans-serif'
+        self.line_height = 20
+        self.mouse_down = False
+        self.mouse_x = 0
+        self.mouse_y = 0
+        self.offsetX = 0
+        self.offsetY = 0
+        self.clientX = 0
+        self.clientY = 0
+        self.screenX = 0
+        self.screenY = 0
+        self.layerX = 0
+        self.layerY = 0
+        self.win_x = 0
+        self.win_y = 0
+        self.pageX = 0
+        self.pageY = 0
+        self.win_left = 0
+        self.win_top = 0
+        self.win_app = ''
+        self.key_down = ' '
+
+    def handle_mouseup(self, event):
+        Scene.handle_mouseup(self, event)
+        self.mouse_down = False
+
+    def handle_mousedown(self, event):
+        Scene.handle_mousedown(self, event)
+        self.mouse_down = True
+
+    def handle_mousemove(self, event):
+        Scene.handle_mousemove(self, event)
+        self.mouse_x = event.x
+        self.mouse_y = event.y
+        self.offsetX = event.offsetX
+        self.offsetY = event.offsetY
+        self.clientX = event.clientX
+        self.clientY = event.clientY
+        self.screenX = event.screenX
+        self.screenY = event.screenY
+        self.layerX = event.layerX
+        self.layerY = event.layerY
+        self.pageX = event.pageX
+        self.pageY = event.pageY
+        self.win_x = event.currentTarget.screenX
+        self.win_y = event.currentTarget.screenY
+        self.win_left = event.currentTarget.screenLeft
+        self.win_top = event.currentTarget.screenTop
+        self.win_app = event.currentTarget.navigator.appCodeName
+        self.win_app_version = event.currentTarget.navigator.appVersion
+
+    def handle_keydown(self, event):
+        Scene.handle_keydown(self, event)
+        self.key_down = event.key
+
+    def draw(self, ctx):
+        Scene.draw(self, ctx)
+        ctx.save()
+        ctx.globalCompositeOperation = 'source-over'
+        # ctx.beginPath()
+        ctx.font = self.font
+        ctx.fillStyle = 'black'
+        ctx.strokeStyle = 'black'
+        ctx.lineWidth = 2
+
+        lh = self.line_height/2
+        ctx.beginPath()
+        ctx.moveTo(self.mouse_x - lh, self.mouse_y - lh)
+        ctx.lineTo(self.mouse_x + lh, self.mouse_y + lh)
+        ctx.moveTo(self.mouse_x + lh, self.mouse_y - lh)
+        ctx.lineTo(self.mouse_x - lh, self.mouse_y + lh)
+        ctx.closePath()
+        ctx.stroke()
+
+        yy = 20
+
+        ctx.fillText('Key down: {}'.format(self.key_down), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('Mouse down: {}'.format(self.mouse_down), 20, yy)
+        yy = yy + self.line_height
+        yy = yy + self.line_height
+
+        ctx.fillText('Mouse: {}, {}'.format(self.mouse_x, self.mouse_y), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('Offset: {}, {}'.format(self.offsetX, self.offsetY), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('Client: {}, {}'.format(self.clientX, self.clientY), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('Layer: {}, {}'.format(self.layerX, self.layerY), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('Page: {}, {}'.format(self.pageX, self.pageY), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('Screen: {}, {}'.format(self.screenX, self.screenY), 20, yy)
+        yy = yy + self.line_height
+        yy = yy + self.line_height
+
+        ctx.fillText('Canvas: {}'.format(self.game.canvas.localName), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('width/height: {}, {}'.format(self.game.canvas.width, self.game.canvas.height), 40, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('left/top: {}, {}'.format(self.game.canvas.offsetLeft, self.game.canvas.offsetTop), 40, yy)
+        yy = yy + self.line_height
+        yy = yy + self.line_height
+
+        ctx.fillText('Window: {}'.format(self.win_app), 20, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('x/y: {}, {}'.format(self.win_x, self.win_y), 40, yy)
+        yy = yy + self.line_height
+
+        ctx.fillText('left/top: {}, {}'.format(self.win_left, self.win_top), 40, yy)
+        yy = yy + self.line_height
+        yy = yy + self.line_height
+
+        ctx.fillText(self.win_app_version, 20, yy)
+
+        ctx.lineWidth = 1
+        ctx.strokeStyle = 'blue'
+        ctx.rect(0, 0, self.game.canvas.width, self.game.canvas.height)
+        ctx.stroke()
+        ctx.strokeStyle = 'green'
+        ctx.beginPath()
+        ctx.moveTo(300, 300 - lh)
+        ctx.lineTo(300, 300 + lh)
+        ctx.moveTo(300 - lh, 300)
+        ctx.lineTo(300 + lh, 300)
+        ctx.closePath()
+        ctx.stroke()
+        ctx.restore()
+
+
+class DemoGame(Game):
     def __init__(self, loop_time=20):
         Game.__init__(self, 'Balls', loop_time)
         self.append(DemoScene(self, 'SCENE', 128))
 
 
-class BallsGame1(Game):
+class DemoGame1(Game):
     def __init__(self, loop_time=20):
         Game.__init__(self, 'Balls', loop_time)
 
         self.scene1 = DemoScene1(self, 'SCENE', 8)
         btn1 = self.scene1.append(Button(self, 50, 300, 130, 30, 'Switch'))
-        btn1.callback = self.hey
+        btn1.callback = self.switch_scene
         self.append(self.scene1)
 
         self.scene2 = DemoScene2(self, 'SCENE2', 64)
         btn2 = self.scene2.append(Button(self, 200, 300, 130, 30, 'Switch'))
-        btn2.callback = self.hey
+        btn2.callback = self.switch_scene
         self.append(self.scene2)
 
         self.scene3 = DemoScene3(self, 'SCENE3', 16)
         btn3 = self.scene3.append(Button(self, 350, 300, 130, 30, 'Switch'))
-        btn3.callback = self.hey
+        btn3.callback = self.switch_scene
         self.append(self.scene3)
 
-        self.currentScene = self.scene1
+        self.scene4 = DemoScene4(self, 'SCENE4')
+        btn4 = self.scene4.append(Button(self, 500, 300, 130, 30, 'Switch'))
+        btn4.callback = self.switch_scene
+        self.append(self.scene4)
 
-    def hey(self):
-        console.log("Hey!!!")
+        self.currentScene = self.scene4
+
+    def switch_scene(self):
+        console.log("Switch!!!")
         self.currentScene = self.scene2 if self.currentScene == self.scene1 else \
-            (self.scene3 if self.currentScene == self.scene2 else self.scene1)
+            (self.scene3 if self.currentScene == self.scene2 else
+             (self.scene4 if self.currentScene == self.scene3 else self.scene1))
