40c89fb22463db47834cf233612416e733a447f6
Keith Rieck <KeithRieck@gmail.com>
2026-09-18 12:57:24 -0500

    Added pointerdown event listener

--- bedlam.py
+++ bedlam.py
@@ -705,6 +705,8 @@ class Game:
         window.addEventListener('keydown', self.handle_keydown, False)
         window.addEventListener('touchstart', self.handle_mousedown, False)
         window.addEventListener('pointerdown', self.handle_mousedown, False)
+        window.addEventListener('pointerup', self.handle_mouseup, False)
+        window.addEventListener('pointermove', self.handle_mousemove, False)
         window.addEventListener('touchend', self.handle_mouseup, False)
         window.addEventListener('touchmove', self.handle_mousemove, False)
         window.addEventListener('gamepadconnected', self._add_gamepad, False)
